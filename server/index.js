2474c59bad22ee8c62561f4c260dc138b78a8840
Josh Genereux <jgenereux@stratacache.com>
2023-12-19 11:27:18 -0500

    cleaned up base app, added some backend stuff

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const PORT = process.env.SERVER_PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
